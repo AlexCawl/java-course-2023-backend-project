0bc417e398a0135c050aa37a5105728219a190b3
AlexCawl <babushkinsusu@gmail.com>
2024-03-31 22:10:51 +0500

    feat: added rate limiting

--- bot/src/main/java/edu/java/bot/di/ApplicationBeanConfig.java
+++ bot/src/main/java/edu/java/bot/di/ApplicationBeanConfig.java
@@ -2,9 +2,11 @@ package edu.java.bot.di;
 
 import com.pengrad.telegrambot.TelegramBot;
 import edu.java.core.util.ApiQualifier;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableCaching
 @Configuration
 public class ApplicationBeanConfig {
     @Bean
